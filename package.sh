f687c70207af768754175a5f55779ef835738c66
MOBIUS <hd2dj07@gmail.com>
2020-04-09 14:54:09 +0900

    package.sh issue fixed

--- package.sh
+++ package.sh
@@ -6,3 +6,4 @@ cd ${SCRIPTDIR}/django-utils/
 python3 setup.py sdist bdist_wheel
 cp ${SCRIPTDIR}/django-utils/dist/django_utils-0.0.1.tar.gz ${SCRIPTDIR}/backend/
 #python3 -m pip install ${SCRIPTDIR}/backend/django_utils-0.0.1.tar.gz
+
